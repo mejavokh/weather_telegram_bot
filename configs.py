cbd653d9452db9c3ef338e79e16350018128d0c9
mejavohk <tavhidovjavohir@gmail.com>
2024-06-08 11:39:39 +0500

    'sc'

--- configs.py
+++ configs.py
@@ -1,3 +1,3 @@
-TOKEN = '6562907681:AAGqyj--5-yvb3tZue1SQ4g_Qc8a_JuqbRw'
-API = 'bae7d1623d8debe7ac6f8f63e55e5f9b'
+TOKEN = '#'  # Вместо решетки поставьте свой токен
+API = '#'  # Вместо решетки поставьте свой апи
 URL = 'https://api.openweathermap.org/data/2.5/weather'
